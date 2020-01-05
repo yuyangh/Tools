378a07ef2db999591f919be66334ccb3cee27ee3
Yuyang Huang <yuyangh@usc.edu>
2020-01-05 12:22:45 +0800

    update playground

--- playground.py
+++ playground.py
@@ -1,21 +1,15 @@
-def calculate_purchase():
-    purchase = {'盛一洲': 10.5,
-                '周金程': 14.5,
-                '朱昶宇': 13,
-                '李杨': 14,
-                '黄郁旸': 14,
-                '于天易': 17 / 2,
-                'Kitty': 17 / 2,
-                '张宇伦': 13}
-    sum = 0
-    tip = 12
-    tax = 0.08
-    for item in purchase:
-        value = purchase[item] * (1 + tax)
-        sum += value
-        print(str(item), str(value + tip / len(purchase)))
-    print(sum + tip)
+def main():
+    n = int(input())
+    m = int(input())
+
+    sum_divisible = 0
+    sum_not_divisible = 0
+
+    if (m >= n):
+        sum_divisible = (1 + m // n) * m // n / 2 * n
+    sum_not_divisible = (1 + m) * m / 2 - sum_divisible
+    print(abs(sum_not_divisible - sum_divisible))
 
 
 if __name__ == '__main__':
-    calculate_purchase()
+    main()
